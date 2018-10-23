7001106e8719b2607a9cfebfc5a818436b8e682f
Arlios <luyen_mai@Live.com>
2018-10-22 19:12:43 -0700

    Some Refactoring and basic images.

--- tmtm-react/src/Header.js
+++ tmtm-react/src/Header.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './Header.css';
 
-class App extends Component {
+class Header extends Component {
   render() {
     return (
       <div className="App">
@@ -17,4 +17,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Header;
